75be4d17e2cfe0a12c59bb0d7e3f57ef41f4b56d
Sajjad Asghar <sajjad.asghar@unb.ca>
2013-09-29 19:54:55 -0300

    DMSAT worker solver 1.0

---
+++ workersolver/core/depend.mk
@@ -0,0 +1,36 @@
+/home/sasghar/test/minisat/core/Main.o /home/sasghar/test/minisat/core/Main.or /home/sasghar/test/minisat/core/Main.od /home/sasghar/test/minisat/core/Main.op: /home/sasghar/test/minisat/core/Main.cc \
+  /home/sasghar/test/minisat/utils/System.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/utils/ParseUtils.h \
+  /home/sasghar/test/minisat/utils/Options.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/mtl/XAlloc.h \
+  /home/sasghar/test/minisat/utils/ParseUtils.h \
+  /home/sasghar/test/minisat/core/Dimacs.h \
+  /home/sasghar/test/minisat/core/SolverTypes.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/mtl/Alg.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/mtl/Map.h \
+  /home/sasghar/test/minisat/mtl/Alloc.h \
+  /home/sasghar/test/minisat/core/Solver.h \
+  /home/sasghar/test/minisat/mtl/Heap.h
+/home/sasghar/test/minisat/core/Solver.o /home/sasghar/test/minisat/core/Solver.or /home/sasghar/test/minisat/core/Solver.od /home/sasghar/test/minisat/core/Solver.op: /home/sasghar/test/minisat/core/Solver.cc \
+  /home/sasghar/test/minisat/mtl/Sort.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/mtl/XAlloc.h \
+  /home/sasghar/test/minisat/core/Solver.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/mtl/Heap.h \
+  /home/sasghar/test/minisat/mtl/Alg.h \
+  /home/sasghar/test/minisat/utils/Options.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/mtl/Vec.h \
+  /home/sasghar/test/minisat/utils/ParseUtils.h \
+  /home/sasghar/test/minisat/core/SolverTypes.h \
+  /home/sasghar/test/minisat/mtl/IntTypes.h \
+  /home/sasghar/test/minisat/mtl/Map.h \
+  /home/sasghar/test/minisat/mtl/Alloc.h
