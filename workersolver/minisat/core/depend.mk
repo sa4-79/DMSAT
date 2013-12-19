f65f31e2dd6b443f22ce0e625a0b6cd50cc2d6c5
Sajjad Asghar <sajjad.asghar@unb.ca>
2013-12-19 14:36:41 -0400

    New version of worker SoLver

---
+++ workersolver/minisat/core/depend.mk
@@ -0,0 +1,40 @@
+/home/sasghar/benchmark/minisat/core/Main.o /home/sasghar/benchmark/minisat/core/Main.or /home/sasghar/benchmark/minisat/core/Main.od /home/sasghar/benchmark/minisat/core/Main.op: /home/sasghar/benchmark/minisat/core/Main.cc \
+  /home/sasghar/benchmark/minisat/utils/System.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/utils/ParseUtils.h \
+  /home/sasghar/benchmark/minisat/utils/Options.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/XAlloc.h \
+  /home/sasghar/benchmark/minisat/utils/ParseUtils.h \
+  /home/sasghar/benchmark/minisat/core/Dimacs.h \
+  /home/sasghar/benchmark/minisat/core/SolverTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/Alg.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/mtl/Map.h \
+  /home/sasghar/benchmark/minisat/mtl/Alloc.h \
+  /home/sasghar/benchmark/minisat/core/Solver.h \
+  /home/sasghar/benchmark/minisat/mtl/Heap.h \
+  /home/sasghar/Zeromq/zmq/include/zhelpers.h \
+  /home/sasghar/Zeromq/zmq/include/zmq.h
+/home/sasghar/benchmark/minisat/core/Solver.o /home/sasghar/benchmark/minisat/core/Solver.or /home/sasghar/benchmark/minisat/core/Solver.od /home/sasghar/benchmark/minisat/core/Solver.op: /home/sasghar/benchmark/minisat/core/Solver.cc \
+  /home/sasghar/benchmark/minisat/mtl/Sort.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/XAlloc.h \
+  /home/sasghar/benchmark/minisat/core/Solver.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/mtl/Heap.h \
+  /home/sasghar/benchmark/minisat/mtl/Alg.h \
+  /home/sasghar/benchmark/minisat/utils/Options.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/Vec.h \
+  /home/sasghar/benchmark/minisat/utils/ParseUtils.h \
+  /home/sasghar/benchmark/minisat/core/SolverTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/IntTypes.h \
+  /home/sasghar/benchmark/minisat/mtl/Map.h \
+  /home/sasghar/benchmark/minisat/mtl/Alloc.h \
+  /home/sasghar/Zeromq/zmq/include/zhelpers.h \
+  /home/sasghar/Zeromq/zmq/include/zmq.h
